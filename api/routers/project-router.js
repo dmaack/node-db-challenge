8fcf76a3c32cb4804dff125d2ba8c4aef86e69d1
dominique maack <dominiquemaack@gmail.com>
2019-11-08 10:25:56 -0700

    set up routers and model file - skeletons

---
+++ api/routers/project-router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Projects = require('../models/project-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+})
+
+router.get('/:id', (req, res) => {
+    
+})
+
+router.post('/', (req, res) => {
+    
+})
+
+router.post('/:id/tasks', (req, res) => {
+    
+})
+
+module.exports = router;
---
+++ api/routers/resources-router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const Resources = require('../models/resource-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+    
+})
+
+
+module.exports = router;
